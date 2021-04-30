4c973060d30ed44ff0c25267e31f3ac0c651bcfe
Carlos Ceron <carlos.ceron.1986@gmail.com>
2021-04-29 21:06:20 -0500

    Finalizacion del componente.

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -1,11 +1,29 @@
-import React, { Fragment } from 'react'
-import PropTypes from 'prop-types'
+import React, { Fragment, useState } from 'react';
+import PropTypes from 'prop-types';
 
 const CounterApp = ({value}) => {
+
+    const [counter, setCounter] = useState(value);
+
+    const handleAdd = (e) => {
+        setCounter(counter+1);
+    }
+
+    const handleSubs = (e) => {
+        setCounter( (counter > 0) ? counter-1 : 0);
+    }
+
+    const handleClean = (e) => {
+        setCounter(0);
+    }
+
     return (
         <Fragment>
             <h1>CounterApp</h1>
-            <h2>{value}</h2>
+            <h2>{counter}</h2>
+            <button  onClick={handleAdd} > + </button>
+            <button  onClick={handleClean} > Clear </button>
+            <button  onClick={handleSubs} > - </button>
         </Fragment>
     )
 }
